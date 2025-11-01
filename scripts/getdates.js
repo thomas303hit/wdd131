f2bbf4036c507c8805d7bf34d93cb6d39d44d729
thomas303hit <thomasdguedes64@gmail.com>
2025-11-01 15:42:27 -0300

    new complete

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,6 +2,4 @@ document.addEventListener('DOMContentLoaded', function() {
     // Set current year
     document.getElementById("currentyear").textContent = new Date().getFullYear();
     
-    // Removed: Set last modified (line commented out or deleted to not show the date)
-    // document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
 });
